079139bc73af1c32e487533ae52dbe883b884700
umair2607 <umair.k2607@gmail.com>
2023-03-18 10:12:33 +0500

    login signup routes

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAiMz2bujhJIaFvlO1i77m9uNlATDzG0g",
+  authDomain: "scholars-hub-d282e.firebaseapp.com",
+  projectId: "scholars-hub-d282e",
+  storageBucket: "scholars-hub-d282e.appspot.com",
+  messagingSenderId: "706259306538",
+  appId: "1:706259306538:web:cedff17c40d131ab5adb53",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Login from "./page/Login";
+import Signup from "./page/Signup";
+import Home from "./page/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <section>
+          <Routes>
+            {" "}
+            <Route path="/">
+              <Route index element={<Login />} />
+              <Route path="signup" element={<Signup />} />
+            </Route>{" "}
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
