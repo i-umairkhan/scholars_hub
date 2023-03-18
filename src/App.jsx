64b5c2eb4e60d39e211a3f6384a3d4b01e1b3b97
umair2607 <umair.k2607@gmail.com>
2023-03-18 10:49:17 +0500

    signup layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./page/Login";
 import Signup from "./page/Signup";
 import Home from "./page/Home";
+import "./App.css";
 
 function App() {
   return (
@@ -11,11 +12,10 @@ function App() {
       <div>
         <section>
           <Routes>
-            {" "}
             <Route path="/">
               <Route index element={<Login />} />
               <Route path="signup" element={<Signup />} />
-            </Route>{" "}
+            </Route>
             <Route path="/home" element={<Home />} />
           </Routes>
         </section>
--- src/page/Signup.jsx
+++ src/page/Signup.jsx
@@ -1,7 +1,38 @@
+import { Button, TextField } from "@mui/material";
 import React from "react";
 
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className=" w-screen h-screen bg-gradient-to-r from-cyan-500 to-blue-500 flex">
+      <div className="w-[50%] flex flex-col justify-center items-center">
+        <h1 className="text-6xl text-white font-bold">Schloars Hub.</h1>
+        <p className="text-white text-2xl">
+          Your big opportunity may be right where you are now
+        </p>
+      </div>
+      <div className="flex flex-col w-[50%]  justify-center items-center bg-white gap-10">
+        <div className="flex flex-col justify-center items-center gap-2">
+          <h1 className="text-3xl font-bold ">SignUp</h1>
+          <h1 className="text-1xl">Create your account</h1>
+        </div>
+        <div className="flex flex-col gap-5 w-[55%]">
+          <TextField
+            id="standard-basic"
+            label="First Name"
+            variant="outlined"
+          />
+          <TextField
+            id="standard-basic"
+            label="Second Name"
+            variant="outlined"
+          />
+          <TextField id="standard-basic" label="Email" variant="outlined" />
+          <TextField id="standard-basic" label="Password" variant="outlined" />
+          <Button variant="contained">Create Account</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
